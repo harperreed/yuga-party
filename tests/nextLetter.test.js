53c68097440ab3965a6983c3dcaf50d650e355bf
Harper Reed <harper@nata2.org>
2025-01-20 17:09:02 -0600

    updated packages

--- tests/nextLetter.test.js
+++
@@ -1,8 +0,0 @@
-import { nextLetter } from '../src/app.js';
-
-test('nextLetter cycles through A, B, C', () => {
-    expect(nextLetter()).toBe('A');
-    expect(nextLetter()).toBe('B');
-    expect(nextLetter()).toBe('C');
-    expect(nextLetter()).toBe('A');
-});
